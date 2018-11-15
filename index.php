bca57e70aafaa7e8b1f6203bab2c874bce4473c0
kristianklimek <klimekkristian@gmail.com>
2018-11-15 10:14:10 +0100

    i commit suicide.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>LOL</title>
+  </head>
+  <body>
+
+  <?php
+
+  for ($i=0; $i < 30 ; $i++) {
+  $age=rand(0,30);
+    if ($age >= 18) {
+      echo $age . "Pojď dále.";
+    }
+  }
+    ?>
+  </body>
+  </html
